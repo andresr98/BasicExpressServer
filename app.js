714a32fe2a4845c1b358c2d34cff06971a6e4842
Andres Ruiz Graciano <aandres.r98@gmail.com>
2021-05-27 18:12:20 -0500

    App.js file now is responsible to connect to database server

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
 const Express = require('express');
 const server = require('./config/server');
+const mongoose = require('mongoose');
+
+// Database configuration values
+const {
+  MONGO_URI,
+  MONGODB_OPTIONS,
+} = require('./db/mongo');
+
+const {
+  DB_DIALECT,
+} = require('./environment');
 
 const app = new Express();
 server(app);
 
 app.listen(app.get('port'), () => {
   console.log(`The app is listening on the port ${app.get('port')}`);
+  
+   if (DB_DIALECT === 'mongo') {
+    mongoose.connect(MONGO_URI, MONGODB_OPTIONS)
+      .then(value => {
+        console.log('Succesfull Mongo database connection!');
+      })
+      .catch(err => {
+        console.log('Error while connecting to Mongo database', err);
+      });
+  }
 });
--- config/server.js
+++ config/server.js
@@ -3,7 +3,6 @@
  */
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-const mongoose = require('mongoose');
 const cors = require('cors');
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
@@ -26,30 +25,11 @@ const {
   morganMode,
 } = require('./config');
 
-const {
-  MONGO_URI,
-  MONGODB_OPTIONS,
-} = require('./db/mongo');
-
-const {
-  DB_DIALECT,
-} = require('./environment');
-
-
 /**
  * Sets up the server configuration to an Express app
  * @param {*} app Basic Express app
  */
 const server = (app) => {
-  if (DB_DIALECT === 'mongo') {
-    mongoose.connect(MONGO_URI, MONGODB_OPTIONS, (err) => {
-      if (err) {
-        return console.log('Error while connecting to Mongo database');
-      }
-      return console.log('Succesfull Mongo database connection!');
-    });
-  }
-
   app.disable('x-powered-by');
   app.set('port', port);
   app.use(bodyParser.json());
